a3a0453c3deaffcc1701f308c174a800447f0181
Neil Judson <shuiyoufeng@yeah.net>
2018-01-09 11:28:23 +0800

    multipath route recognize and analyse multipath route

--- route-compare.py
+++ route-compare.py
@@ -4,7 +4,7 @@
 # Copyright by Neil Judson
 # Revision: 0.1 Date: 2018/01/08 20:00:00
 
-import sys
+# import sys
 import re
 import chardet
 
@@ -25,8 +25,8 @@ class RouteCompare:
         try:
             s = f.read()
             encode_type = chardet.detect(s)['encoding']
-        except:
-            print('Wrong: 1')
+        except Exception as e1:
+            print('ERROR1: ', e1)
             exit(-1)
         finally:
             f.close()
@@ -38,57 +38,85 @@ class RouteCompare:
             exit(-1)
         try:
             s = f.readline()
-            net_mask = '/32'
+            s_net_ip = '0.0.0.0/32'
+            s_net_mask = '/32'
             dic_route_table = {}
             while s:
                 ip = re.search(self.ip_reg, s)
                 if ip:
                     if re.search('is[a-z ]*subnetted', s):
-                        net_mask_0 = re.search(r'/[1-3]?\d', ip.group())
-                        if net_mask_0:
-                            net_mask = net_mask_0.group()
+                        # subnetted net
+                        net_mask = re.search(r'/[1-3]?\d', ip.group())
+                        if net_mask:
+                            s_net_mask = net_mask.group()
+                    elif re.search('^ *\[\d*/\d*\]', s):
+                        # multipath route
+                        dic_route_detail = self.get_route_detail(s)  # analyse route entry
+                        dic_route_detail.update({'Type': dic_route_table[s_net_ip]['Type']})
+                        dic_route_table.update({s_net_ip: [dic_route_table[s_net_ip], dic_route_detail]})
                     else:
+                        # route
                         sub_net_mask = re.search(r'/[1-3]?\d', ip.group())
                         if sub_net_mask:
-                            net_ip = ip.group()
+                            s_net_ip = ip.group()
                         else:
-                            net_ip = ip.group() + net_mask
-                        route_detail = self.get_route_detail(s)  # 解析出route_detail
-                        dic_route_table.setdefault(net_ip, route_detail)
+                            s_net_ip = ip.group() + s_net_mask
+                        dic_route_detail = self.get_route_detail(s)  # analyse route entry
+                        dic_route_table.update({s_net_ip: dic_route_detail})
                 s = f.readline()
             return dic_route_table
-        except:
-            print('Wrong: 2')
+        except Exception as e2:
+            print('ERROR1: ', e2)
             exit(-1)
         finally:
             f.close()
 
     def get_route_detail(self, s):
-        route_detail = ''
+        dic_route_detail = {}
         dic_route_message_reg = {
-            'Type':         r'^[a-zA-Z]*',
-            'AD/Metric':    r'\[\d*\/\d*\]',
-            'Interface':    r'((FastEthernet|Ethernet|Serial)\d*\/\d*)|null0'
+            'Type': r'^[a-zA-Z]*',
+            'AD/Metric': r'\[\d*/\d*\]',
+            'Interface': r'((FastEthernet|Ethernet|Serial)\d*/\d*)|null0'
         }
         for i in dic_route_message_reg:
             route_message = re.search(dic_route_message_reg[i], s)
             if route_message:
-                route_detail = route_detail + i + ':' + route_message.group() + ' | '
+                dic_route_detail.update({i: route_message.group()})
             else:
-                route_detail = route_detail + i + ':' + ' | '
-        next_hop_0 = re.search(re.compile(r'via ' + self.s_ip_reg), s)
+                dic_route_detail.update({i: ''})
+        next_hop_0 = re.search(r'via ' + self.s_ip_reg, s)
         if next_hop_0:
             next_hop = re.search(self.ip_reg, next_hop_0.group())
             if next_hop:
-                route_detail = route_detail + 'NextHop:' + next_hop.group() + ' | '
+                dic_route_detail.update({'NextHop': next_hop.group()})
             else:
-                route_detail = route_detail + 'NextHop:' + ' | '
-        # interface = re.search(r'((FastEthernet|Ethernet|Serial)\d*\/\d*)|null0', s)
-        # if interface:
-        #     route_detail = route_detail + 'Interface:' + interface.group() + ' | '
-        # else:
-        #     route_detail = route_detail + 'Interface:' + ' | '
-        return route_detail
+                dic_route_detail.update({'NextHop': ''})
+        return dic_route_detail
+        # route_detail = ''
+        # dic_route_message_reg = {
+        #     'Type':         r'^[a-zA-Z]*',
+        #     'AD/Metric':    r'\[\d*/\d*\]',
+        #     'Interface':    r'((FastEthernet|Ethernet|Serial)\d*/\d*)|null0'
+        # }
+        # for i in dic_route_message_reg:
+        #     route_message = re.search(dic_route_message_reg[i], s)
+        #     if route_message:
+        #         route_detail = route_detail + i + ':' + route_message.group() + ' | '
+        #     else:
+        #         route_detail = route_detail + i + ':' + ' | '
+        # next_hop_0 = re.search(re.compile(r'via ' + self.s_ip_reg), s)
+        # if next_hop_0:
+        #     next_hop = re.search(self.ip_reg, next_hop_0.group())
+        #     if next_hop:
+        #         route_detail = route_detail + 'NextHop:' + next_hop.group()
+        #     else:
+        #         route_detail = route_detail + 'NextHop:'
+        # # interface = re.search(r'((FastEthernet|Ethernet|Serial)\d*\/\d*)|null0', s)
+        # # if interface:
+        # #     route_detail = route_detail + 'Interface:' + interface.group() + ' | '
+        # # else:
+        # #     route_detail = route_detail + 'Interface:' + ' | '
+        # return route_detail
 
     def compare_route_table(self, dic_table_1, dic_table_2):
         return dic_table_1 == dic_table_2
@@ -98,9 +126,8 @@ if __name__ == '__main__':
     rc = RouteCompare()
     dic_route_table_1 = rc.read_route_file('R1.log')
     for j in dic_route_table_1:
-        print('%-18s' % j + ' ' + dic_route_table_1[j])
-    print()
-    print([(k,  dic_route_table_1[k]) for k in sorted(dic_route_table_1.keys())])  # 排序
-    dic_route_table_2 = rc.read_route_file('R2.log')
+        print('%-18s' % j + ' ', dic_route_table_1[j])
+    print([(k,  dic_route_table_1[k]) for k in sorted(dic_route_table_1.keys())])  # sort
+    dic_route_table_2 = rc.read_route_file('R1.log')
     result = rc.compare_route_table(dic_route_table_1, dic_route_table_2)
     print(result)
