353d508f85108467f09afe743b5adf95aa38ed9b
Neil Judson <shuiyoufeng@yeah.net>
2018-08-22 01:47:29 +0800

    no message

--- test/test_route_compare.py
+++ test/test_route_compare.py
@@ -9,8 +9,12 @@ from route_compare import *
 
 
 def main():
-    route_table_old = get_route_table(sys.argv[3], sys.argv[1])
-    route_table_new = get_route_table(sys.argv[3], sys.argv[2])
+    try:
+        route_table_old = get_route_table(sys.argv[3], sys.argv[1])
+        route_table_new = get_route_table(sys.argv[3], sys.argv[2])
+    except Exception as e:
+        print(e)
+        return
     result = compare_route_table(route_table_old, route_table_new)
     show_result_table(result, sys.argv[1], sys.argv[2])
     print(result_to_web(result))
