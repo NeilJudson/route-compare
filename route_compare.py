75448f6c1f68d7643af87249b3dfbd4941a0fa02
Neil Judson <shuiyoufeng@yeah.net>
2018-08-31 01:10:37 +0800

    no message

--- route_compare.py
+++ route_compare.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Python 3.6
 
 import os
 import re
--- test/test_route_compare.py
+++ test/test_route_compare.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# Python 3.6
 
 import sys
 
@@ -9,14 +10,22 @@ from route_compare import *
 
 
 def main():
+    if len(sys.argv) == 4:
+        file_name_old = sys.argv[1]
+        file_name_new = sys.argv[2]
+        route_os = sys.argv[3]
+    else:
+        file_name_old = 'show_ip_route_C7KA.txt'
+        file_name_new = 'show_ip_route_C7KB.txt'
+        route_os = 'NXOS'
     try:
-        route_table_old = get_route_table(sys.argv[3], sys.argv[1])
-        route_table_new = get_route_table(sys.argv[3], sys.argv[2])
+        route_table_old = get_route_table(route_os, file_name_old)
+        route_table_new = get_route_table(route_os, file_name_new)
     except Exception as e:
         print(e)
         return
     result = compare_route_table(route_table_old, route_table_new)
-    show_result_table(result, sys.argv[1], sys.argv[2])
+    show_result_table(result, file_name_old, file_name_new)
     print(result_to_web(result))
     sys.exit()
 
